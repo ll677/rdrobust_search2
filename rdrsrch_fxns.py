e58e725f942cb498341da33e10f20ad5beb7d49c
Linchen Zhang <lz266@cornell.edu>
2019-06-26 15:04:00 -0400

    change typos in getURL

--- rdrsrch_fxns.py
+++ rdrsrch_fxns.py
@@ -25,9 +25,9 @@ def getURLs(username, password, owner):
         username: email address to login to bitbucket
         password: password to login to bit bitbucket
         owner: owner of bitucket repo, i.e. aeaverification in this case
-        
+
     Returns:
-    
+
         URLs: dict; URLs of repos to clone index tuples, 0 index is string date
             of last update, 1 index is string name of repo
     """
@@ -37,9 +37,9 @@ def getURLs(username, password, owner):
     checked_URLs_time = list(df['last_updated_time'])
     client = Client(str(username), str(password), str(owner))
     # repos = client.get_repositories()['values']
-    
+
     # alt soln. here: https://thepythoncoding.blogspot.com/2019/06/python-script-to-clone-all-repositories.html?fbclid=IwAR0a-cI-EI9cA1cgQGkiXCY9R6-5SrJq_NItKurEQ59eSVnzGCVpmKtWs7g
-    
+
     # compile repo list
     pg=1
     d=[0]
@@ -48,25 +48,25 @@ def getURLs(username, password, owner):
         d=client.get_repositories(params={'pagelen':100,'page':pg})['values']
         repos=repos+d
         pg+=1
-    
+
     #get URLs
     for repo in repos:
         links = repo['links']
         clone = links['clone']
         URL = clone[0]['href']
-        upd = repo['updated_on'])
+        upd_time = repo['updated_on'])
         name = repo['name']
         if URL in checked_URLs:
             index = checked_URLs.index(URL)
-            old_upd = checked_URLs_time[index]
-            if parseTime(old_time) < parseTime(upd):
+            old_time = checked_URLs_time[index]
+            if parseTime(old_time) < parseTime(upd_time):
                 URLs[URL]=(upd,name)
         else:
             URLs[URL]=(upd,name)
     return URLs
 
 
-def cloneRepos(URLs): 
+def cloneRepos(URLs):
 
     """
     Clones the repos corresponding to the links in URLs into the a subdirectory
@@ -82,11 +82,11 @@ def cloneRepos(URLs):
         repos: a dictionary indexing the repos' URLs to a tuple of their repo object (0),
             last update time (1), and DOI if present (2) ('' if not present)
 
-        
+
     """
-    
+
     repos={}
-    
+
     #create and navigate to new directory repos
     try:
         os.mkdir('repos')
@@ -104,7 +104,7 @@ def cloneRepos(URLs):
         upd=URLs[url][0]
         name=URLs[url][1]
         r=git.Repo.clone_from(url,os.getcwd()+'\\'+name)
-        
+
         #Try to get DOI
         DOI=''
         suffSt=name.find('10.1257-')+8
@@ -122,7 +122,7 @@ def cloneRepos(URLs):
     return repos
 
 
-def rdrobustOccurrences(repos): 
+def rdrobustOccurrences(repos):
 
     """
     Input:
@@ -149,35 +149,35 @@ def rdrobustOccurrences(repos):
             ct+=count_rdrobust(text)
         if ct>0:
             new_counts.at[url]={'DOI':repos[url][2],'rdr_counts':ct}
-            
+
     return new_counts
 
 def update_rdr_counts(new_counts):
-    
+
     """
     Updates rdr_counts.csv in current working directory with info in new_counts. Includes
     entries for new repos and replaces entries for old repos which have been modified.
-    
+
     Input:
-    
+
         new_counts: A pd.DataFrame (index are URLs, col 1 is DOI if extractable, col 2 is #
             of rdrobust occurrences)
-    
+
     """
     old_counts=pd.read_csv('rdr_counts.csv',index_col='URL')
     old_counts.update(new_counts[new_counts.index.isin(old_counts.index)])
     rdr_counts=old_counts.append(new_counts[~new_counts.index.isin(old_counts.index)])
     rdr_counts.to_csv('rdr_counts.csv')
-    
-    
+
+
 def update_checked_URL(repos):
 
     """
     Updates checked_URL.csv with the URLs and last modified date of repos examined in this
     run of the script.
-    
+
     Input:
-    
+
         repos: a dictionary indexing the repos' URLs to a tuple of their repo object (0),
             last update time (1), and DOI if present (2) ('' if not present)
     """
@@ -192,34 +192,34 @@ def update_checked_URL(repos):
 # getURLs helpers
 
 def parseTime(s):
-    
+
     """
     Input:
-    
+
         s: a string representing a date and time in the form:
             <YYYY>-<MM>-<DD>T<hh>:<mm>:<ss>.<ssssss>+00:00
-    
+
     Returns:
-        
+
         t: a datetime object representing the date and time given by s
     """
-    
+
     hyp1=s.find('-')
     hyp2=s.find('-',hyp1+1)
     Tpos=s.find('T')
     col1=s.find(':')
     col2=s.find(':',col1+1)
     plus=s.find('+')
-    
+
     year=int(s[:hyp1])
     month=int(s[hyp1+1:hyp2])
     day=int(s[hyp2+1:Tpos])
     hour=int(s[Tpos+1:col1])
     minute=int(s[col1+1:col2])
     seconds=int(s[col2+1:plus])
-    
+
     t=dt.datetime(year,month,day,hour,minute,seconds)
-    
+
     return t
 
 # rdrobustOccurrences helpers
