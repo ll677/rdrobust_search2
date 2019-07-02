0f5dbdba0563c99602f1fd2ea635eec03942b508
Linchen Zhang <lz266@cornell.edu>
2019-07-02 13:06:03 -0400

    fix new updating issues

--- rdrsrch_fxns.py
+++ rdrsrch_fxns.py
@@ -1,4 +1,4 @@
-#functions for the rdrobust_search script
+# functions for the rdrobust_search script
 
 import os
 import git
@@ -23,43 +23,44 @@ def getURLs(username, password, owner, redo=False):
         username: email address to login to bitbucket
         password: password to login to bit bitbucket
         owner: owner of bitucket repo, i.e. aeaverification in this case
-		redo: if True, then redo all URL repos cloning disregarding the checked_URLS.csv
+                redo: if True, then redo all URL repos cloning disregarding the checked_URLS.csv
     Returns:
 
         URLs: dict; URLs of repos to clone index tuples, 0 index is string date
             of last update, 1 index is string name of repo
     """
-    URLs={}
+    URLs = {}
 # change to "checked_URL_empty.csv" if you want to start from fresh
     if not redo:
-    	df = pd.read_csv("checked_URL.csv")
+        df = pd.read_csv("checked_URL.csv")
 
-    checked_URLs= list(df['URL'])
+    checked_URLs = list(df['URL'])
     checked_URLs_time = list(df['last_updated_time'])
     client = Client(str(username), str(password), str(owner))
 
     # alt soln. here: https://thepythoncoding.blogspot.com/2019/06/python-script-to-clone-all-repositories.html?fbclid=IwAR0a-cI-EI9cA1cgQGkiXCY9R6-5SrJq_NItKurEQ59eSVnzGCVpmKtWs7g
 
     # compile repo list
-    pg=1
-    d=[0]
-    repos=[]
-    while len(d)>0:
-        d=client.get_repositories(params={'pagelen':100,'page':pg})['values']
-        repos=repos+d
-        pg+=1
+    pg = 1
+    d = [0]
+    repos = []
+    while len(d) > 0:
+        d = client.get_repositories(
+            params={'pagelen': 100, 'page': pg})['values']
+        repos = repos+d
+        pg += 1
 
-    #get URLs
+    # get URLs
 
     for repo in repos:
         links = repo['links']
         clone = links['clone']
         raw_URL = clone[0]['href']
-        
-        #remove user handle from url
-        hst=raw_URL.find('//')+2
-        hend=raw_URL.find('bitbucket.org')
-        URL=raw_URL[:hst]+raw_URL[hend:]
+
+        # remove user handle from url
+        hst = raw_URL.find('//')+2
+        hend = raw_URL.find('bitbucket.org')
+        URL = raw_URL[:hst]+raw_URL[hend:]
 
         upd_time = repo['updated_on']
         name = repo['name']
@@ -67,14 +68,13 @@ def getURLs(username, password, owner, redo=False):
             index = checked_URLs.index(URL)
             old_time = checked_URLs_time[index]
             if parseTime(old_time) < parseTime(upd_time):
-                URLs[URL]=(upd_time,name)
+                URLs[URL] = (upd_time, name)
         else:
-            URLs[URL]=(upd_time,name)
+            URLs[URL] = (upd_time, name)
     return URLs
 
 
 def cloneRepos(URLs):
-
     """
     Clones the repos corresponding to the links in URLs into the a subdirectory
     named repos of the current working directory. May require manual login.
@@ -92,37 +92,53 @@ def cloneRepos(URLs):
 
     """
 
-    repos={}
+    repos = {}
 
-    #create and navigate to new directory repos
+    # create and navigate to new directory repos
 
     try:
         os.mkdir('repos')
     except FileExistsError:
         pass
 
-    current_dir=os.getcwd()
+    current_dir = os.getcwd()
 
     os.chdir('repos')
 
-    #add repos to folder named repos
+    # add repos to folder named repos
 
     for url in URLs.keys():
 
         print('cloning '+str(url))
-        upd=URLs[url][0]
-        name=URLs[url][1]
-        DOI=findDOI(name)
-        try:
-        	r=git.Repo.clone_from(url,os.getcwd()+'\\'+name)
-        	repos[url]=(r,upd,DOI)
-        except:
-        	r = git.Repo(os.getcwd()+'\\'+name)
-        	o = r.remotes.origin
-        	o.pull()
-        	repos[url]=(r,upd,DOI)
+        upd = URLs[url][0]
+        name = URLs[url][1]
+        DOI = findDOI(name)
 
-    #navigate back to original directory
+        # git clone if directory doesn't exist
+
+        try:
+            r = git.Repo.clone_from(url, os.getcwd()+'\\'+name)
+            repos[url] = (r, upd, DOI)
+
+        except:
+
+            # try git pull if directory already exists
+
+            try:
+                print('try to pull'+str(os.getcwd()+'\\'+name))
+                r = git.Repo(os.getcwd()+'\\'+name)
+                o = r.remotes.origin
+                o.pull()
+                repos[url] = (r, upd, DOI)
+
+            #encount empty uniniciated repo
+            except:
+                print('just continue:'+str(os.getcwd()+'\\'+name))
+                r = git.Repo(os.getcwd()+'\\'+name)
+                repos[url] = (r, upd, DOI)
+                continue
+
+    # navigate back to original directory
 
     os.chdir(current_dir)
 
@@ -130,7 +146,6 @@ def cloneRepos(URLs):
 
 
 def rdrobustOccurrences(repos):
-
     """
     Input:
 
@@ -143,25 +158,25 @@ def rdrobustOccurrences(repos):
             of rdrobust occurrences)
     """
 
-    new_counts=pd.DataFrame(index=pd.core.indexes.base.Index([],name='URL'),
-                            columns=['DOI','rdr_counts'])
-    URLs=repos.keys()
+    new_counts = pd.DataFrame(index=pd.core.indexes.base.Index([], name='URL'),
+                              columns=['DOI', 'rdr_counts'])
+    URLs = repos.keys()
     for url in URLs:
-        ct=0
+        ct = 0
         paths = getFilePaths(repos[url][0].working_dir)
         for f in paths:
-            file=open(f,'r', errors='ignore')
-            text=file.read()
+            file = open(f, 'r', errors='ignore')
+            text = file.read()
             file.close()
-            ct+=count_rdrobust(text)
-        if ct>0:
+            ct += count_rdrobust(text)
+        if ct > 0:
 
-            new_counts.at[url]={'DOI':repos[url][2],'rdr_counts':ct}
+            new_counts.at[url] = {'DOI': repos[url][2], 'rdr_counts': ct}
 
     return new_counts
 
-def update_rdr_counts(new_counts):
 
+def update_rdr_counts(new_counts):
     """
     Updates rdr_counts.csv in current working directory with info in new_counts. Includes
     entries for new repos and replaces entries for old repos which have been modified.
@@ -172,14 +187,14 @@ def update_rdr_counts(new_counts):
             of rdrobust occurrences)
 
     """
-    old_counts=pd.read_csv('rdr_counts.csv',index_col='URL')
+    old_counts = pd.read_csv('rdr_counts.csv', index_col='URL')
     old_counts.update(new_counts[new_counts.index.isin(old_counts.index)])
-    rdr_counts=old_counts.append(new_counts[~new_counts.index.isin(old_counts.index)])
+    rdr_counts = old_counts.append(
+        new_counts[~new_counts.index.isin(old_counts.index)])
     rdr_counts.to_csv('rdr_counts.csv')
 
 
 def update_checked_URL(repos):
-
     """
     Updates checked_URL.csv with the URLs and last modified date of repos examined in this
     run of the script.
@@ -189,9 +204,9 @@ def update_checked_URL(repos):
         repos: a dictionary indexing the repos' URLs to a tuple of their repo object (0),
             last update time (1), and DOI if present (2) ('' if not present)
     """
-    checked_URL=pd.read_csv('checked_URL.csv',index_col='URL')
+    checked_URL = pd.read_csv('checked_URL.csv', index_col='URL')
     for url in repos.keys():
-        checked_URL.at[url]={'last_updated_time':repos[url][1]}
+        checked_URL.at[url] = {'last_updated_time': repos[url][1]}
     checked_URL.to_csv('checked_URL.csv')
 
 
@@ -200,7 +215,6 @@ def update_checked_URL(repos):
 # getURLs helpers
 
 def parseTime(s):
-
     """
     Input:
 
@@ -212,24 +226,25 @@ def parseTime(s):
         t: a datetime object representing the date and time given by s
     """
     # print(s)
-    hyp1=s.find('-')
-    hyp2=s.find('-',hyp1+1)
-    Tpos=s.find('T')
-    col1=s.find(':')
-    col2=s.find(':',col1+1)
-    plus=s.find('+')
+    hyp1 = s.find('-')
+    hyp2 = s.find('-', hyp1+1)
+    Tpos = s.find('T')
+    col1 = s.find(':')
+    col2 = s.find(':', col1+1)
+    plus = s.find('+')
 
-    year=int(s[:hyp1])
-    month=int(s[hyp1+1:hyp2])
-    day=int(s[hyp2+1:Tpos])
-    hour=int(s[Tpos+1:col1])
-    minute=int(s[col1+1:col2])
-    seconds=int(float(s[col2+1:plus]))
+    year = int(s[:hyp1])
+    month = int(s[hyp1+1:hyp2])
+    day = int(s[hyp2+1:Tpos])
+    hour = int(s[Tpos+1:col1])
+    minute = int(s[col1+1:col2])
+    seconds = int(float(s[col2+1:plus]))
 
-    t=dt.datetime(year,month,day,hour,minute,seconds)
+    t = dt.datetime(year, month, day, hour, minute, seconds)
     return t
 
 # cloneRepos helper
+
 
 def findDOI(name):
     """
@@ -241,19 +256,20 @@ def findDOI(name):
 
         DOI: string of the repo's DOI if extractable, '' otherwise
     """
-    DOI=''
-    prefEnd=name.find('10.1257')+7
+    DOI = ''
+    prefEnd = name.find('10.1257')+7
 
     if prefEnd < 0:
         return DOI
 
     import string
     try:
-        suffSt=re.search(str(list(string.ascii_letters)),name[prefEnd:]).start()+prefEnd
-        suffEnd=name.find('-',suffSt)
+        suffSt = re.search(str(list(string.ascii_letters)),
+                           name[prefEnd:]).start()+prefEnd
+        suffEnd = name.find('-', suffSt)
         if suffEnd < 0:
-            suffEnd=len(name)
-        DOI='10.1257/'+name[suffSt:suffEnd]
+            suffEnd = len(name)
+        DOI = '10.1257/'+name[suffSt:suffEnd]
         return DOI
     except:
         return DOI
@@ -262,28 +278,27 @@ def findDOI(name):
 # rdrobustOccurrences helpers
 
 def getFilePaths(dir):
+    """
+    Input:
 
-        """
-        Input:
+        dir: the path (as a string) to a directory to search
 
-            dir: the path (as a string) to a directory to search
+    Returns:
 
-        Returns:
+        paths: a list of do and R filepaths contained in dir and its
+            subdirectories
+    """
 
-            paths: a list of do and R filepaths contained in dir and its
-                subdirectories
-        """
+    dirtree = os.walk(dir)
+    paths = []
+    for t in dirtree:
+        for f in t[2]:
+            if f[-3:] == '.do' or f[-2:] == '.R':
+                paths = paths + [os.path.join(t[0], f)]
+    return paths
 
-        dirtree=os.walk(dir)
-        paths=[]
-        for t in dirtree:
-            for f in t[2]:
-                if f[-3:]=='.do' or f[-2:]=='.R':
-                    paths = paths + [os.path.join(t[0],f)]
-        return paths
 
 def count_rdrobust(text):
-
     """
     Input:
 
@@ -294,5 +309,5 @@ def count_rdrobust(text):
         count: occurrances of rdrobust in the text
     """
 
-    count=sum(1 for i in re.finditer('rdrobust',text))
+    count = sum(1 for i in re.finditer('rdrobust', text))
     return count
